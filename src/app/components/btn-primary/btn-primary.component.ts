38d3054eae38ac1ef59552482f85f7a712859f54
EnzoSantoss <enzoaugusto312@gmail.com>
2024-03-05 20:11:44 -0300

    adicionando botão

--- src/app/components/btn-primary/btn-primary.component.ts
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 type BtnVarient = 'primary' | 'secondery';
 
 @Component({
   selector: 'app-btn-primary',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './btn-primary.component.html',
   styleUrl: './btn-primary.component.scss',
 })
